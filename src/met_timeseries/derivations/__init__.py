552eb8e5f988f4af92e05f49e9b1b6abd5e427d4
Mulu Fratkin <Michael.Fratkin@state.mn.us>
2026-04-24 15:07:28 -0500

    Rename Old Derivations module
    renamed derivations module to derivations_legacy
    checked new derivation package paths work against a previous workflow.

--- src/met_timeseries/derivations/__init__.py
+++ src/met_timeseries/derivations/__init__.py
@@ -4,7 +4,6 @@ Source-agnostic derived variable calculations.
 
 from met_timeseries.derivations.wind import wind_speed, adjust_wind_height
 from met_timeseries.derivations.humidity import (
-    saturation_vapor_pressure_magnus,
     specific_humidity,
     relative_humidity,
     relative_humidity_from_specific_humidity,
@@ -35,32 +34,3 @@ from met_timeseries.derivations.pet import (
     pet_penman_hourly,
     pet_penman_monteith_hourly,
 )
-
-__all__ = [
-    "wind_speed",
-    "adjust_wind_height",
-    "saturation_vapor_pressure_magnus",
-    "specific_humidity",
-    "relative_humidity",
-    "relative_humidity_from_specific_humidity",
-    "dewpoint_from_specific_humidity",
-    "dewpoint_from_specific_humidity_cc",
-    "dewpoint_august_roche_magnus",
-    "kelvin_to_fahrenheit",
-    "kelvin_to_celsius",
-    "net_radiation",
-    "clearsky_radiation",
-    "clearsky_array",
-    "cloud_cover_davis",
-    "sky_cover_radiation_thompson",
-    "cloud_cover_thompson",
-    "cloud_cover_linear",
-    "clearsky_radiation_hww",
-    "actual_radiation_hww",
-    "pet_hargreaves",
-    "pet_penman_pyet_daily",
-    "pet_penman_pyet_hourly",
-    "pet_penman_kohler",
-    "pet_penman_hourly",
-    "pet_penman_monteith_hourly",
-]
